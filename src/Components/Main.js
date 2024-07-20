eda794ffb6b532ad007e71a19989dd6b9b516856
iulisesq <iulisesq@hotmail.com>
2024-07-20 17:40:40 -0400

    updating the grid

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -33,7 +33,7 @@ const Main = () => {
   return (
     <Container>
       {loggedInUser ? (
-          <ReportComponent />
+          <Operacion />
         )
        : (
         <form onSubmit={handleLogin}>
